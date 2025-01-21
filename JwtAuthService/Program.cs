f291cb23b61022bdcd95c4f6107ada858cf17170
gadopyatikna <dunno@donny-2.local>
2025-01-21 18:19:21 +0100

    add sln

--- JwtAuthService/Program.cs
+++ JwtAuthService/Program.cs
@@ -14,53 +14,11 @@ var secretKey = "my-very-long-token-suoer-puper-secret-key";
 // Add services
 builder.Services.AddSingleton(new AuthService(secretKey));
 
-// Add JWT authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.UseSecurityTokenValidators = true;
-    options.IncludeErrorDetails = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey)),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 // Add Swagger services
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UseSwagger();  // Enable Swagger
