0d6241e2979b27b75f93b32a7964b7de46e7a4a5
gadopyatikna <dunno@donny-2.local>
2025-01-21 18:17:11 +0100

    extract protected endpoints into service

--- ProtectedService/Controllers/ProtectedController.cs
+++ ProtectedService/Controllers/ProtectedController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace JwtAuthService.Controllers;
+namespace ProtectedService.Controllers;
 
 [ApiController]
 [Route("[controller]")]
---
+++ ProtectedService/Program.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.OpenApi.Models;
+
+IdentityModelEventSource.ShowPII = true;
+var builder = WebApplication.CreateBuilder(args);
+
+// Secret key for JWT
+var secretKey = "my-very-long-token-suoer-puper-secret-key";
+
+// Add JWT authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.UseSecurityTokenValidators = true;
+    options.IncludeErrorDetails = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+// Add Swagger services
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+var app = builder.Build();
+app.UseSwagger();  // Enable Swagger
+app.UseSwaggerUI();  // Enable Swagger UI
+
+// Use authentication and authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
